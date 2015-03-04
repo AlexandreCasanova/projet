36fd9a3bfc08230e4f408ecb277c9f7ef6ad9565
DuryAlexandre <alexandredury@hotmail.fr>
2015-03-04 13:37:37 +0100

    git commit

--- classe_basique.py
+++ classe_basique.py
@@ -131,7 +131,33 @@ class Degager(SoccerStrategy) :
         else :
             return 1
 
-# Modifier l'angle par le joueur le plus proche.
+
+class GoalStrategy(SoccerStrategy):
+    def __init__(self):
+        self.name="Goal"
+    def start_battle(self,state):
+        pass
+    def finish_battle(self,won):
+        pass
+    def compute_strategy(self,state,player,teamid):
+        diff = state.ball.position - player.position
+        tir = Vector2D(0,0)
+        if diff.norm > 5 :
+            vitesse = state.ball.position + state.get_goal_center(teamid) - player.position - player.position
+        else :
+            vitesse = state.ball.position - player.position
+        if player.position.distance(state.ball.position)<(PLAYER_RADIUS+BALL_RADIUS) :    
+            tir = state.get_goal_center(self.team_adverse(teamid)) - player.position
+        return SoccerAction(vitesse,tir)
+    def copy(self):
+        return GoalStrategy()
+    def create_strategy(self):
+        return GoalStrategy()
+    def team_adverse(self,teamid):
+        if (teamid==1):
+            return 2
+        else :
+            return 1
    
 
 class ComposeStrategy(SoccerStrategy):
